1061edd10abdb89ee8a1441b4b63dbdff2ac727f
JORDAN MANLEY <jordanmanley@JORDANs-MacBook-Pro.local>
2023-05-09 15:46:54 -0400

    sort tags alphabetically, ignoring emojis

--- src/routes/resources/+page.server.ts
+++ src/routes/resources/+page.server.ts
@@ -2,28 +2,48 @@ import type { PageServerLoad } from './$types'
 import { parse } from 'yaml'
 import fs from 'fs'
 
-let resources_path: string = "data/resources.yml"
+interface Resource {
+  title: string,
+  description: string,
+  url: string,
+  tags: string[]
+}
+
+let resourcesPath: string = "data/resources.yml"
 
 // Reading in resources
-const file = fs.readFileSync(resources_path, 'utf8')
-const yml_data = parse(file) // TODO: Create interface for this, and then use it to validate edits during CI/CD
+const file = fs.readFileSync(resourcesPath, 'utf8')
+const resources: Resource[] = parse(file) // TODO: Use interface to validate edits during CI/CD
 
-// Creating a list of unique tags
-let tags: string[] = [];
-let resource;
-for (resource of yml_data) {
-  tags.push(...resource.tags)
+
+const generateUniquetags = (resources: Resource[]) => {
+  const tags: string[] = []
+
+  for (const resource of resources) {
+    tags.push(...resource.tags);
+  }
+
+  return [...new Set(tags)]
 }
-tags = [...new Set(tags)]
 
-//Sorting resources by newest (assuming newest is at the bottom of the file )
-const yml_data_sort = [...yml_data].reverse();
+const sortAlphabeticallyIgnoringEmojis = (a: string, b: string) => {
+  const aWithoutEmojis = a.replace(/[\u1000-\uFFFF]+/g, '').trim();
+  const bWithoutEmojis = b.replace(/[\u1000-\uFFFF]+/g, '').trim();
+
+  return aWithoutEmojis.localeCompare(bWithoutEmojis);
+}
+
+const uniqueTags: string[] = generateUniquetags(resources);
+
+const uniqueTagsInAlphabeticalOrder = uniqueTags.sort(sortAlphabeticallyIgnoringEmojis);
+
+const resourcesNewestToOldest = [...resources].reverse();
 
 export function load(params: PageServerLoad) {
   return {
     payload: {
-      resources: yml_data_sort,
-      tags: tags,
+      resources: resourcesNewestToOldest,
+      tags: uniqueTagsInAlphabeticalOrder,
     }
   }
 }
